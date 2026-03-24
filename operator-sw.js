763701578473b47b4812803adb9f5402616b29b6
Sistema Rids <juancarlosofcial@gmail.com>
2026-03-24 14:46:14 -0300

    correção

--- operator-sw.js
+++ operator-sw.js
@@ -1,4 +1,4 @@
-const OPERATOR_CACHE = "checkmaint-operator-v1";
+const OPERATOR_CACHE = "checkmaint-operator-v2";
 
 const CORE_ASSETS = [
   "./operador.html",
@@ -6,12 +6,71 @@ const CORE_ASSETS = [
   "./assets/pwa/checkmaint-icon.svg",
   "./assets/pwa/checkmaint-icon-maskable.svg",
   "./xl_media_image4.png",
-  "./assets/js/defs/index.js"
+  "./assets/js/defs/index.js",
+  "./assets/js/defs/caminhao-basculante.js",
+  "./assets/js/defs/caminhao-carroceria.js",
+  "./assets/js/defs/caminhao-comboio.js",
+  "./assets/js/defs/caminhao-munck.js",
+  "./assets/js/defs/caminhao-pipa.js",
+  "./assets/js/defs/carregadeira.js",
+  "./assets/js/defs/cavalo-mecanico.js",
+  "./assets/js/defs/checklists.js",
+  "./assets/js/defs/compressor.js",
+  "./assets/js/defs/empilhadeira.js",
+  "./assets/js/defs/escavadeira.js",
+  "./assets/js/defs/gerador-energia.js",
+  "./assets/js/defs/mini-carregadeira.js",
+  "./assets/js/defs/motoniveladora.js",
+  "./assets/js/defs/onibus.js",
+  "./assets/js/defs/perfuratriz.js",
+  "./assets/js/defs/prancha.js",
+  "./assets/js/defs/retroescavadeira.js",
+  "./assets/js/defs/rompedor.js",
+  "./assets/js/defs/trator-esteira.js",
+  "./assets/js/defs/veiculos-leves.js",
+  "./assets/icons/equipment/backhoe.svg",
+  "./assets/icons/equipment/breaker.svg",
+  "./assets/icons/equipment/bulldozer.svg",
+  "./assets/icons/equipment/bus.svg",
+  "./assets/icons/equipment/car.svg",
+  "./assets/icons/equipment/compressor.svg",
+  "./assets/icons/equipment/crane-truck.svg",
+  "./assets/icons/equipment/drill-rig.svg",
+  "./assets/icons/equipment/dump-truck.svg",
+  "./assets/icons/equipment/excavator.svg",
+  "./assets/icons/equipment/flatbed-truck.svg",
+  "./assets/icons/equipment/forklift.svg",
+  "./assets/icons/equipment/generator.svg",
+  "./assets/icons/equipment/grader.svg",
+  "./assets/icons/equipment/loader.svg",
+  "./assets/icons/equipment/semi-truck.svg",
+  "./assets/icons/equipment/service-truck.svg",
+  "./assets/icons/equipment/trailer.svg",
+  "./assets/icons/equipment/water-truck.svg"
+];
+
+const EXTERNAL_ASSETS = [
+  "https://cdn.tailwindcss.com/3.4.17",
+  "https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js",
+  "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth-compat.js",
+  "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore-compat.js",
+  "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(OPERATOR_CACHE).then((cache) => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting())
+    caches.open(OPERATOR_CACHE).then(async (cache) => {
+      await Promise.allSettled(
+        CORE_ASSETS.map((asset) => cache.add(asset))
+      );
+
+      await Promise.allSettled(
+        EXTERNAL_ASSETS.map(async (asset) => {
+          const response = await fetch(asset, { mode: "no-cors" });
+          await cache.put(asset, response);
+        })
+      );
+    }).then(() => self.skipWaiting())
   );
 });
 
@@ -31,7 +90,6 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
   const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin) return;
 
   const isNavigation = event.request.mode === "navigate";
 
